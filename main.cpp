d6c204d32ca0c3646ca094c3ff4b4e9436c5f5e7
Abdellah Azeroual <abazerou@e3r7p11.1337.ma>
2024-08-15 14:37:00 +0100

    fixed empty passord and space in password

--- main.cpp
+++ main.cpp
@@ -2,20 +2,16 @@
 #include <cstdlib>
 #include <sys/signal.h>
 #include "server.hpp"
-// #include <poll.h>
 
-void f() {
-    system("leaks ircserv");
-}
+
 
 int main(int ac , char** av)
 {
-    atexit(f);
     if (ac != 3) {
         std::cerr << "Usage: " << av[0] << " <port>" << " <password>"<< std::endl;
         exit(1);
     }
-    if(!isdigit(*av[1]))
+    if(!isdigit(*av[1]) || std::string(av[2]).find(" ") != std::string::npos || std::string(av[2]).length() < 1)
     {
         std::cerr << "Invalid port number" << std::endl;
         exit(1);
